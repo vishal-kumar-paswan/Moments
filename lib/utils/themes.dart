dfd03d427e38ba13f4d6406e31bfae335144ba4e
Vishal Kumar Paswan <kumarpaswanvishal@gmail.com>
2021-05-20 23:32:24 +0530

    custom theme added

---
+++ lib/utils/themes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MomentsThemeData {
+  static ThemeData lightTheme = ThemeData(
+    fontFamily: GoogleFonts.lato().fontFamily,
+  );
+}
