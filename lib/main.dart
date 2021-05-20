cc7ef197d12e229fbe4fc112e62fb274025848a2
Vishal Kumar Paswan <kumarpaswanvishal@gmail.com>
2021-05-21 04:21:39 +0530

    code refactored, added routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:moments/screens/main_screen.dart';
 import 'package:moments/screens/messages_screen.dart';
+import 'package:moments/utils/routes.dart';
 import 'package:moments/utils/themes.dart';
 
 void main() => runApp(Moments());
@@ -10,7 +12,11 @@ class Moments extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: MomentsThemeData.lightTheme,
-      home: MessagesScreen(),
+      initialRoute: MomentsRoutes.homeRoute,
+      routes: {
+        MomentsRoutes.homeRoute: (context) => MainScreen(),
+        MomentsRoutes.messagesRoute: (context) => MessagesScreen(),
+      },
     );
   }
 }
