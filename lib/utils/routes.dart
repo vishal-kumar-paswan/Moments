05cdff08eda94a9e5c874c4b3211feef7debe3b5
Vishal Kumar Paswan <kumarpaswanvishal@gmail.com>
2021-06-02 20:36:46 +0530

    added signup route

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -2,4 +2,5 @@ class MomentsRoutes {
   static String mainScreenRoute = "/mainscreen";
   static String splashScreenRoute = "/splashscreen";
   static String loginRoute = "/login";
+  static String signUpRoute = "/signup";
 }
