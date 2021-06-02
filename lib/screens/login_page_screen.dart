298524a40baf0b1010a80a388c800b5a559d9172
Vishal Kumar Paswan <kumarpaswanvishal@gmail.com>
2021-06-02 20:36:16 +0530

    removed Custom Clipper class

--- lib/screens/login_page_screen.dart
+++ lib/screens/login_page_screen.dart
@@ -1,7 +1,8 @@
 import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:moments/utils/routes.dart';
+import 'package:moments/widgets/custom_clipper.dart';
 
 class LoginPage extends StatelessWidget {
   @override
@@ -232,7 +233,8 @@ class LoginPage extends StatelessWidget {
                 children: [
                   Text('New to Moments?'),
                   TextButton(
-                    onPressed: () => print('SignUp'),
+                    onPressed: () =>
+                        Navigator.pushNamed(context, MomentsRoutes.signUpRoute),
                     child: Text(
                       'Register',
                       style: TextStyle(
@@ -247,32 +249,5 @@ class LoginPage extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class MyCustomClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    Path path = Path();
-    path.lineTo(0.0, size.height - 30);
-
-    var firstControlPoint = Offset(size.width / 4, size.height);
-    var firstPoint = Offset(size.width / 2, size.height);
-    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
-        firstPoint.dx, firstPoint.dy);
-
-    var secondControlPoint = Offset(size.width - (size.width / 4), size.height);
-    var secondPoint = Offset(size.width, size.height - 30);
-    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
-        secondPoint.dx, secondPoint.dy);
-
-    path.lineTo(size.width, 0.0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return false;
   }
 }
